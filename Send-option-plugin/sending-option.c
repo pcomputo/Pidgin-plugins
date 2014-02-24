5c8d2c68412e08401234b7845e50457a44fc2f20
pcomputo <ahuja.pooja22@gmail.com>
2014-02-24 16:32:34 +0530

    Removing unwanted headers

--- Send-option-plugin/sending-option.c
+++ Send-option-plugin/sending-option.c
@@ -1,21 +1,23 @@
-
+/*
+ * WSend option - Helps you send various chat entities to your buddies.
+ * Copyright (C) 2013 Pooja Ahuja <ahuja.pooja22@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
 #include "internal.h"
 #include "version.h"
-//#include "pidgin.h"
-
-/*#include "connection.h"
-#include "debug.h"
-#include "prefs.h"
-#include "notify.h"
-#include "signals.h"
-#include "util.h"
-
-
-#include "gtkblist.h"
-#include "gtkconv.h"
-#include "gtkplugin.h"
-#include "gtkutils.h"
-#include "gtkdialogs.h"*/
 
 /* This file defines PURPLE_PLUGINS and includes all the libpurple headers */
 #include "purple.h"
